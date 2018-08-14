2d2c95ac8b471f90d133a5f10aa33ca81242fad2
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-14 21:29:34 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
-class Model
-  # Replace with Dog clas
+ require 'ApplicationController' Sinatra::Base
+class Dog 
+attr_accessor :name. :breed, :age
+Dogs = []
+ def initialize(name, breed, age)
+   @name => name
+   @breed => breed
+   @age = > age
+   Dogs << self
+ end
+  def self .assert_select_all
+    Dogs
+  end
 end
